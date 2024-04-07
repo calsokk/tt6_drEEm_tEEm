b248d9dfeb6e8a4e29d8b0012ea19c4a8628757c
William Zhu <williamzhu@DN0a22a926.SUNet>
2024-04-07 11:35:33 -0700

    started position aim

---
+++ src/pos_aim.v
@@ -0,0 +1,15 @@
+module pos_aim (
+    input wire clk,
+    input wire reset,
+    input wire left_x,
+    input wire right_x,
+    input wire left_aim,
+    input wire right_aim,
+    output wire [4:0] x_pos,
+    output wire [2:0] aim_pos
+);
+
+    // x_pos
+    dffr #()
+
+endmodule
